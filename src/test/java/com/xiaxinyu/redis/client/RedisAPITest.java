ca315300cdc63919233f39972d435e1b9e638a43
summer.xia <summer_west2010@126.com>
2020-07-26 10:31:40 +0800

    add RedisAPI

--- src/test/java/com/xiaxinyu/redis/client/RedisAPITest.java
+++ src/test/java/com/xiaxinyu/redis/client/RedisAPITest.java
@@ -1,5 +1,6 @@
 package com.xiaxinyu.redis.client;
 
+import com.xiaxinyu.redis.client.api.RedisAPI;
 import com.xiaxinyu.redis.client.model.Request;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
@@ -11,17 +12,21 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.concurrent.TimeUnit;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
 public class RedisAPITest {
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    RedisTemplate redisTemplate;
 
+    @Autowired
+    RedisAPI redisApi;
 
     @Test
-    public void testSet() {
+    public void test_OpsForValue() {
         Request request = Request.builder().id("001").content("I love Summer").build();
 
         ValueOperations<String, Request> operations = redisTemplate.opsForValue();
@@ -33,5 +38,16 @@ public class RedisAPITest {
         Request req = (Request) redisTemplate.opsForValue().get(request.getId());
 
         Assert.assertTrue(req.equals(request));
+    }
+
+    @Test
+    public void test_expireAt() throws InterruptedException {
+        redisApi.expireKey("001", 10, TimeUnit.SECONDS);
+
+        Thread.sleep(12000);
+
+        boolean exists = redisApi.hasKey("001");
+
+        Assert.assertFalse(exists);
     }
 }
