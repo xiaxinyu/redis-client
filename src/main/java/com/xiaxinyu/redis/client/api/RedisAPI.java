3c3573bc9b0650fa7fe748e2a70fbc205bd4490f
summer.xia <summer_west2010@126.com>
2020-07-26 11:39:05 +0800

    add redis lock2

--- src/main/java/com/xiaxinyu/redis/client/api/RedisAPI.java
+++ src/main/java/com/xiaxinyu/redis/client/api/RedisAPI.java
@@ -1,13 +1,11 @@
 package com.xiaxinyu.redis.client.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.Date;
-import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -136,31 +134,5 @@ public class RedisAPI {
      */
     public void persistKey(String key) {
         redisTemplate.persist(key);
-    }
-
-    public boolean lock(String key) {
-        String lock = LOCK_PREFIX + key;
-
-        // 利用lambda表达式
-        return (Boolean) redisTemplate.execute((RedisCallback) connection -> {
-            long expireAt = System.currentTimeMillis() + LOCK_EXPIRE + 1;
-            Boolean acquire = connection.setNX(lock.getBytes(), String.valueOf(expireAt).getBytes());
-
-            if (acquire) {
-                return true;
-            } else {
-                byte[] value = connection.get(lock.getBytes());
-                if (Objects.nonNull(value) && value.length > 0) {
-                    long expireTime = Long.parseLong(new String(value));
-                    // 如果锁已经过期
-                    if (expireTime < System.currentTimeMillis()) {
-                        // 重新加锁，防止死锁
-                        byte[] oldValue = connection.getSet(lock.getBytes(), String.valueOf(System.currentTimeMillis() + LOCK_EXPIRE + 1).getBytes());
-                        return Long.parseLong(new String(oldValue)) < System.currentTimeMillis();
-                    }
-                }
-            }
-            return false;
-        });
     }
 }
